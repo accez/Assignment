9516ef1350f1b7571c1264e89ac9c5a6313625db
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-17 16:15:07 +0100

    refactored work-button eventlistner it now works with the Work class

--- Komputer Store/main.js
+++ Komputer Store/main.js
@@ -3,3 +3,11 @@ import Work from "./Work.js";
 
 const dataController = new DataController();
 const work = new Work(dataController);
+
+const getWorkButtonId = document.querySelector("[id=work-button]");
+const getPaymentId = document.querySelector("[id=payment]");
+
+getWorkButtonId.addEventListener("click", () => {
+    work.work();
+    getPaymentId.innerHTML = `${dataController.workBalance} kr`;
+});
