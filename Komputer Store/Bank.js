177f10619a2a60e359f242cafefaec215de35676
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-18 09:00:56 +0100

    added getALoan function to Bank.JS it's now refactored and working

---
+++ Komputer Store/Bank.js
@@ -0,0 +1,34 @@
+import { displayMessage } from "./helper.js";
+
+class Bank {
+    constructor(props) {
+        this.props = props;
+    }
+
+    getALoan() {
+        if (this.props.gotBankLoan) {
+            displayMessage(`You may not have two loans at once. The initial loan should be paid back in full`, true);
+            return;
+        } else {
+            let lentMoney = Number(prompt("How much?"));
+            let maxLoan = this.props.bankBalance * 2;
+
+            if (lentMoney === 0) {
+                displayMessage("Loan amount can't be 0", true);
+                return;
+            }
+            if (lentMoney > maxLoan) {
+                displayMessage(`You cannot get a loan more than double of your bank balance. Max loan: ${maxLoan}`, true);
+            }
+            if (lentMoney <= maxLoan) {
+                this.props.bankBalance += lentMoney;
+                this.props.loanBalance += lentMoney;
+                this.props.gotBankLoan = true;
+            } else {
+                this.props.gotBankLoan = false;
+            }
+        }
+    }
+}
+
+export default Bank;
--- Komputer Store/main.js
+++ Komputer Store/main.js
@@ -1,22 +1,23 @@
 import DataController from "./DataController.js";
 import Work from "./Work.js";
+import Bank from "./Bank.js";
 
 const dataController = new DataController();
 const work = new Work(dataController);
+const bank = new Bank(dataController);
 
 const getWorkButtonId = document.querySelector("[id=work-button]");
 const getPaymentId = document.querySelector("[id=payment]");
 const getTransferToBankButtonId = document.querySelector("[id=transfer-money-button]");
 const getBankBalanceId = document.querySelector("[id=bank-balance]");
 const getLoanBalance = document.querySelector("[id=loan-balance]");
-
+const getALoanButtonId = document.querySelector("[id=get-loan-button]");
 
 
 
 
 getWorkButtonId.addEventListener("click", () => {
     work.work();
-    console.log(dataController.workBalance);
     getPaymentId.innerHTML = `${dataController.workBalance} kr`;
 });
 
@@ -28,4 +29,10 @@ getTransferToBankButtonId.addEventListener("click", () => {
     getPaymentId.innerHTML = `${dataController.workBalance} kr`;
     getBankBalanceId.innerHTML = `${dataController.bankBalance} kr`;
     getLoanBalance.innerHTML = `${dataController.loanBalance} kr`;
+});
+
+getALoanButtonId.addEventListener("click", () => {
+    bank.getALoan();
+    getBankBalanceId.innerHTML = `${dataController.bankBalance}kr`;
+    getLoanBalance.innerHTML = `${dataController.loanBalance}kr`;
 });
