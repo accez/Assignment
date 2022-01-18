83d6ad333e54a1424f228a1b3f02215827800d21
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-18 08:19:04 +0100

    transferMoney now refactored and working

--- Komputer Store/Work.js
+++ Komputer Store/Work.js
@@ -7,6 +7,27 @@ class Work {
         this.props.workBalance += 100;
     }
 
+    transferMoney() {
+        let deduction = this.props.workBalance * 0.1;
+        let remainingAfterDeduction = this.props.workBalance - deduction;
+        let remainingLoan = this.props.workBalance - this.props.loanBalance;
+
+        if (this.props.gotBankLoan && deduction > this.props.loanBalance) {
+            this.props.bankBalance += remainingLoan;
+            this.props.loanBalance = 0;
+            this.props.workBalance = 0;
+        } else if (this.props.gotBankLoan) {
+            this.props.loanBalance -= deduction;
+            this.props.bankBalance += remainingAfterDeduction;
+            this.props.workBalance = 0;
+        } else {
+            this.props.bankBalance += this.props.workBalance;
+            this.props.workBalance = 0;
+        }
+        if (this.props.loanBalance === 0) {
+            this.props.gotBankLoan = false;
+        }
+    }
 }
 
 export default Work;
--- Komputer Store/main.js
+++ Komputer Store/main.js
@@ -6,8 +6,26 @@ const work = new Work(dataController);
 
 const getWorkButtonId = document.querySelector("[id=work-button]");
 const getPaymentId = document.querySelector("[id=payment]");
+const getTransferToBankButtonId = document.querySelector("[id=transfer-money-button]");
+const getBankBalanceId = document.querySelector("[id=bank-balance]");
+const getLoanBalance = document.querySelector("[id=loan-balance]");
+
+
+
+
 
 getWorkButtonId.addEventListener("click", () => {
     work.work();
+    console.log(dataController.workBalance);
     getPaymentId.innerHTML = `${dataController.workBalance} kr`;
+});
+
+getTransferToBankButtonId.addEventListener("click", () => {
+    if (dataController.workBalance === 0) {
+        return;
+    }
+    work.transferMoney();
+    getPaymentId.innerHTML = `${dataController.workBalance} kr`;
+    getBankBalanceId.innerHTML = `${dataController.bankBalance} kr`;
+    getLoanBalance.innerHTML = `${dataController.loanBalance} kr`;
 });
