c3eb65939d000a996aa6017f65c07e20dca0edd6
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-18 14:02:51 +0100

    refactor: fixImageError now a helper function

--- Komputer Store/helper.js
+++ Komputer Store/helper.js
@@ -1,6 +1,14 @@
 export function displayMessage(text, errorMessage) {
     const getMessageId = document.querySelector("[id=message]");
-
-    errorMessage ? getMessageId.setAttribute("class", "error-message") : getMessageId.setAttribute("id", "completed-purchase");
+    errorMessage ? getMessageId.setAttribute("class", "error-message") : getMessageId.setAttribute("class", "completed-purchase");
     getMessageId.innerHTML = text;
+}
+
+export function fixImageError(image, format) {
+    let removeJPG = image.split(".");
+    removeJPG.pop();
+    removeJPG.push(format);
+    console.log(removeJPG);
+    let correctImageUrl = removeJPG.join("");
+    return correctImageUrl;
 }
