4186abc017cead5649d2988faa1778cd8a6a0511
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-19 12:59:27 +0100

    fix(Bank): :bug: Loan can not be negative values

--- component/Bank.js
+++ component/Bank.js
@@ -4,7 +4,6 @@ class Bank {
     constructor(props) {
         this.props = props;
     }
-
     getALoan() {
         if (this.props.gotBankLoan) {
             displayMessage(`You may not have two loans at once. The initial loan should be paid back in full`, true);
@@ -13,12 +12,13 @@ class Bank {
             let lentMoney = Number(prompt("How much?"));
             let maxLoan = this.props.bankBalance * 2;
 
-            if (lentMoney === 0) {
-                displayMessage("Loan amount can't be 0", true);
+            if (lentMoney <= 0) {
+                displayMessage("Loan amount can't be less then or equal to 0", true);
                 return;
             }
             if (lentMoney > maxLoan) {
                 displayMessage(`You cannot get a loan more than double of your bank balance. Max loan: ${maxLoan}`, true);
+                return;
             }
             if (lentMoney <= maxLoan) {
                 this.props.bankBalance += lentMoney;
